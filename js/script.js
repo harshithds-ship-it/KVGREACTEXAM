b3aadc73b62b6fd1a25935c54c8bdaa74df4361f
Harshith D S <harshithds@kvgce.ac.in>
2026-06-23 10:54:36 +0530

    cors error fix

--- js/script.js
+++ js/script.js
@@ -5,7 +5,16 @@
  */
 
 const IMAGES = [
-  {"url":"https://picsum.photos/1920/1080?random","author":"Harshith D S","caption":"Random Image"},
+  {"url":"https://picsum.photos/id/1015/1920/1080","author":"Harshith D S","caption":"Mountain Lake Vista"},
+  {"url":"https://picsum.photos/id/1018/1920/1080","author":"Harshith D S","caption":"Misty Mountain Range"},
+  {"url":"https://picsum.photos/id/1016/1920/1080","author":"Harshith D S","caption":"Road Through Autumn Woods"},
+  {"url":"https://picsum.photos/id/1011/1920/1080","author":"Harshith D S","caption":"Man with Canoe at Sunset"},
+  {"url":"https://picsum.photos/id/1012/1920/1080","author":"Harshith D S","caption":"Bird on Fountain Edge"},
+  {"url":"https://picsum.photos/id/1013/1920/1080","author":"Harshith D S","caption":"Mountain Fisherman"},
+  {"url":"https://picsum.photos/id/1014/1920/1080","author":"Harshith D S","caption":"Coastal Pine Tree"},
+  {"url":"https://picsum.photos/id/1019/1920/1080","author":"Harshith D S","caption":"Alpine Lake Reflection"},
+  {"url":"https://picsum.photos/id/1020/1920/1080","author":"Harshith D S","caption":"Dog on Boat"},
+  {"url":"https://picsum.photos/id/1021/1920/1080","author":"Harshith D S","caption":"Black and White Flower"},
 ];
 
 const img = document.getElementById('fullscreen-image');
